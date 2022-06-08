c208b8aa4c7d0cb66c0d1e479f7d523e772e8c5d
MaK <muminkhan2000@gmail.com>
2022-06-08 20:25:42 +0530

    deployment

--- app.js
+++ app.js
@@ -1,10 +1,16 @@
 const express = require('express')
 const app = express()
 
+app.get('/health', (req, res) => {
+  res.send('ok')
+})
 // Heroku dynamically sets a port
 const PORT = process.env.PORT || 5000
 
+
 app.use(express.static('dist'))
+
+
 
 app.listen(PORT, () => {
   // eslint-disable-next-line no-console

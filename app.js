b74c4bd6ed080eeccf4ff7fcc966138c8ca25c47
MaK <muminkhan2000@gmail.com>
2022-06-11 14:54:41 +0530

    #skip

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ app.get('/health', (req, res) => {
   res.send('ok')
 })
 app.get('/version', (req, res) => {
-  res.send('6')
+  res.send('7')
 })
 // Heroku dynamically sets a port
 const PORT = process.env.PORT || 5000
